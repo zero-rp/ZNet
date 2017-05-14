ddb0a9fa048b006c64f6de7995cfd8615f6035a1
冉朋 <22249030@qq.com>
2017-05-14 23:41:29 +0800

    add vsproject

---
+++ tool/config.lua
@@ -0,0 +1,13 @@
+root = "./"
+thread = 8
+logger = nil
+harbor = 0
+--address = "127.0.0.1:2526"
+--master = "127.0.0.1:2013"
+start = "main"	-- main script
+bootstrap = "snlua bootstrap"	-- The service for bootstrap
+--standalone = "0.0.0.0:2013"
+luaservice = root.."service/?.lua;"..root.."test/?.lua;"..root.."examples/?.lua"
+lualoader = "lualib/loader.lua"
+snax = root.."examples/?.lua;"..root.."test/?.lua"
+cpath = root.."cservice/?.so"
